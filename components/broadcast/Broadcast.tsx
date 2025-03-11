1c810d7ee70d78a690420ada10da8d7ab2d5c277
aldoht <aldofernandoht@gmail.com>
2025-03-11 10:38:16 -0600

    update: add props and styling to broadcast and pads

--- components/broadcast/Broadcast.tsx
+++ components/broadcast/Broadcast.tsx
@@ -1,9 +1,27 @@
-import { Text } from "react-native";
+import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
 
-export default function Broadcast() {
+type broadcastProps = {
+    bgColor: string
+}
+
+const Broadcast = (props: broadcastProps)=> {
     return (
-        <div>
+        <View style={[styles.container, { backgroundColor: props.bgColor }]}>
             <Text>Broadcast works correctly!</Text>
-        </div>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: '80%',
+        height: '40%',
+        padding: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 15
+    }
+})
+
+export default Broadcast;
--- components/pads/Pads.tsx
+++ components/pads/Pads.tsx
@@ -1,20 +1,31 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet, View} from "react-native";
 import DpadY from "../dpady/DpadY";
 import DpadX from "../dpadx/DpadX";
+import {PadStateColor} from "../../interfaces/padStateColor";
 
-export default function Pads() {
+type padProps = {
+    colorsY: PadStateColor,
+    colorsX: PadStateColor
+}
+
+const Pads = (props: padProps) => {
     return (
-        <div style={styles.controller}>
-            <DpadY></DpadY>
-            <DpadX></DpadX>
-        </div>
+        <View style={styles.controller}>
+            <DpadY colors={props.colorsY}></DpadY>
+            <DpadX colors={props.colorsX}></DpadX>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     controller: {
+        width: '100%',
+        height: '40%',
         display: 'flex',
+        flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center'
     }
 })
+
+export default Pads;
