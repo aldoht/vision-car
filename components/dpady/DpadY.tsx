5dd3e5be8c358b505cd91a36b9c657ed8745fbc6
aldoht <aldofernandoht@gmail.com>
2025-03-13 23:01:50 -0600

    feat(dpady): create and use svg elements for dpad-y

--- components/dpady/DpadY.tsx
+++ components/dpady/DpadY.tsx
@@ -1,23 +1,132 @@
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, View} from "react-native";
+import Svg, { SvgProps, Path } from "react-native-svg"
 import { PadStateColor } from "../../interfaces/padStateColor";
 
 type padProps = {
     colors: PadStateColor
 }
 
+const PadTopSvg = (props: SvgProps) => (
+    <Svg
+        width={183.231}
+        height={274.454}
+        viewBox="0 0 48.48 72.616"
+        {...props}
+    >
+        <Path
+            d="M80.643 15.72h47.497a7.937 7.937 45 0 1 7.937 7.938v80.453a.035.035 135 0 1-.035.035H72.705V23.658a7.937 7.937 135 0 1 7.938-7.938z"
+            style={{
+                fill: "#000",
+                fillOpacity: 0.0547619,
+                stroke: "#000",
+                strokeWidth: 1.02331,
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+            }}
+            transform="matrix(.75285 0 0 .81182 -54.351 -12.346)"
+        />
+        <Path
+            d="M105 28.772 93.013 40.759h23.974Z"
+            style={{
+                fill: "#000",
+                fillOpacity: 0.0547619,
+                stroke: "#000",
+                strokeWidth: 0.8,
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+            }}
+            transform="translate(-80.76 -19.8)"
+        />
+        <Path
+            d="m88.258 62.918 12.872 22.295M121.767 62.918l-12.872 22.295"
+            style={{
+                fill: "#000",
+                fillOpacity: 0.0547619,
+                stroke: "#000",
+                strokeWidth: 0.799999,
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+            }}
+            transform="translate(-80.76 -19.8)"
+        />
+    </Svg>
+)
+
+const PadBottomSvg = (props: SvgProps) => (
+    <Svg
+        width={183.231}
+        height={274.454}
+        viewBox="0 0 48.48 72.616"
+        {...props}
+    >
+        <Path
+            d="M80.643 15.72h47.497a7.937 7.937 45 0 1 7.937 7.938v80.453a.035.035 135 0 1-.035.035H72.705V23.658a7.937 7.937 135 0 1 7.938-7.938z"
+            style={{
+                fill: "#000",
+                fillOpacity: 0.0547619,
+                stroke: "#000",
+                strokeWidth: 1.02331,
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+            }}
+            transform="matrix(-.75285 0 0 -.81182 102.831 84.962)"
+        />
+        <Path
+            d="m104.89 163.443 11.987-11.987H92.903Z"
+            style={{
+                fill: "#000",
+                fillOpacity: 0.0547619,
+                stroke: "#000",
+                strokeWidth: 0.8,
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+            }}
+            transform="translate(-80.65 -99.798)"
+        />
+        <Path
+            d="m121.632 129.296-12.872-22.294M88.123 129.296l12.872-22.294"
+            style={{
+                fill: "#000",
+                fillOpacity: 0.0547619,
+                stroke: "#000",
+                strokeWidth: 0.799999,
+                strokeDasharray: "none",
+                strokeOpacity: 1,
+            }}
+            transform="translate(-80.65 -99.798)"
+        />
+    </Svg>
+)
+
 const DpadY = (props: padProps)=> {
     return (
-        <View style={[styles.container, {backgroundColor: props.colors.idle}]}>
-            <Text>DpadY is working!</Text>
+        <View style={styles.container}>
+            <View style={[styles.pad, styles.padTop, {backgroundColor: props.colors.idle}]}>
+                <PadTopSvg height={150} width={100.143}></PadTopSvg>
+            </View>
+            <View style={[styles.pad, styles.padBottom, {backgroundColor: props.colors.idle}]}>
+                <PadBottomSvg height={150} width={100.143}></PadBottomSvg>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        padding: 20,
+        gap: 20
+    },
+    pad: {
         alignItems: 'center',
         justifyContent: 'center',
+        borderRadius: 15
+    },
+    padTop: {
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0
+    },
+    padBottom: {
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0
     }
 })
 
